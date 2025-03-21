f907a3d9586945ca8c728beb5829410326c146d3
shivam <shivamdldav@gmail.com>
2025-03-21 15:22:53 +0530

    Initial commit

---
+++ Blind75/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Blind75.Questions;
+
+Console.WriteLine("Hello, World!");
+
+FindMinimumInRotatedSortedArray obj = new FindMinimumInRotatedSortedArray();    
+var c = obj.FindMinimum([4,5,6,7,0,1,2,3]);
+Console.WriteLine($"Min: {c}");
---
+++ Blind75/Questions/FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,37 @@
+namespace Blind75.Questions;
+
+
+/// <summary>
+/// leetcode link : https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+/// explanation for the algo : https://takeuforward.org/data-structure/minimum-in-rotated-sorted-array/
+/// Question: Given an integer array arr of size N, sorted in ascending order (with distinct values).
+/// Now the array is rotated between 1 to N times which is unknown. Find the minimum element in the array.
+/// Example: Input Format:  array = [4,5,6,7,0,1,2,3]
+/// Result: 0
+/// Explanation:  Here, the element 0 is the minimum element in the array.
+/// </summary>
+public class FindMinimumInRotatedSortedArray
+{
+    public int FindMinimum(int[] array)
+    {
+        var min = int.MaxValue;
+        int lo =0, hi = array.Length - 1;
+
+        while (lo <= hi)
+        {
+            var mid = (lo + hi) / 2;
+            if (array[lo] <= array[mid])  //this means minimum is in right half. This condition tells us the left half is the sorted subarray hence we are remo
+            {
+                min = Math.Min(min, array[lo]);
+                lo = mid + 1;
+            }
+            else  // this means minimum is in left half
+            {
+                min = Math.Min(min, array[mid]);   
+                hi = mid - 1;
+            }
+        }
+
+        return min;
+    }
+}
