69dd893bd48a860e47dc619472907fd43335d902
shivam <shivamdldav@gmail.com>
2025-05-01 16:38:10 +0530

    updating Program.cs

---
+++ EasyProblems/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EasyProblems.Questions;
+
+// FindSquareRoot findSquareRoot = new FindSquareRoot();
+// Console.WriteLine(findSquareRoot.MySqrt(4));
+
+BinaryTreePostorderTraversal.TreeNode root = new BinaryTreePostorderTraversal.TreeNode(1);
+root.left = new BinaryTreePostorderTraversal.TreeNode(2);
+root.right = new BinaryTreePostorderTraversal.TreeNode(3);
+
+root.left.left = new BinaryTreePostorderTraversal.TreeNode(4);
+root.left.right = new BinaryTreePostorderTraversal.TreeNode(5);
+
+root.right.right = new BinaryTreePostorderTraversal.TreeNode(8);
+
+root.left.right.left = new BinaryTreePostorderTraversal.TreeNode(6);
+root.left.right.right = new BinaryTreePostorderTraversal.TreeNode(7);
+
+root.left.right.left.left = new BinaryTreePostorderTraversal.TreeNode(9);
+
+BinaryTreePreorderTraversal obj = new BinaryTreePreorderTraversal();  
+var result = obj.PreorderTraversal(root);
+
+foreach (var x in result)
+{
+    Console.WriteLine(x);
+}
